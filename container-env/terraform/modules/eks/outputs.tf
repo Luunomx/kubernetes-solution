6089d130188f1524a7c6ccf052a14a48b2f13618
Hugo Hemlin <hugohemlin@hotmail.com>
2025-10-24 13:53:40 +0200

    feat(gitops): add kustomization.yaml in apps directory to enable App-of-Apps structure

--- container-env/terraform/modules/eks/outputs.tf
+++ container-env/terraform/modules/eks/outputs.tf
@@ -26,3 +26,13 @@ output "node_role_arn" {
   description = "IAM role ARN associated with the EKS worker nodes"
   value       = aws_iam_role.eks_node_role.arn
 }
+
+output "oidc_provider_url" {
+  description = "OIDC issuer URL for the EKS cluster"
+  value       = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+output "oidc_provider_arn" {
+  description = "ARN of the IAM OIDC provider associated with the EKS cluster"
+  value       = aws_iam_openid_connect_provider.eks.arn
+}
